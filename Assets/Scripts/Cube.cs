924671824f3bf801485f12d36ad7c845dfc77dcd
MuhammetFatihYilmaz <muhammet.yilmaz.1995@gmail.com>
2022-06-30 13:30:51 +0300

    Added Stacking and Following to last cube mechanics.

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    [SerializeField] private float followSpeed;
+
+    public void UpdateCubePosition(Transform followedCube, bool isFollowStart)
+    {
+        StartCoroutine(StartFollowingToLastCubePosition(followedCube, isFollowStart));
+    }
+
+    IEnumerator StartFollowingToLastCubePosition(Transform followedCube, bool isFollowStart)
+    {
+
+        while (isFollowStart)
+        {
+            yield return new WaitForEndOfFrame();
+            transform.position = new Vector3(Mathf.Lerp(transform.position.x, followedCube.position.x, followSpeed * Time.deltaTime),
+                transform.position.y,
+                Mathf.Lerp(transform.position.z, followedCube.position.z, followSpeed * Time.deltaTime));
+        }
+    }
+}
---
+++ Assets/Scripts/Stacking.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stacking : MonoBehaviour
+{
+    private Rigidbody _playerRigidbody;
+    private float _xMove;
+    private float _zMove;
+    private Vector3 _firstCubePos;
+    private Vector3 _currentCubePos;
+    //
+    [SerializeField] private float _speed;
+    //
+    List<GameObject> _cubeList = new List<GameObject>();
+    private int _cubeListIndexCounter = 0;
+
+    private void Awake()
+    {
+        _playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        _xMove = Input.GetAxis("Horizontal");
+        _zMove = Input.GetAxis("Vertical");
+        Vector3 forwardMove = Vector3.forward * _zMove *_speed * Time.deltaTime;
+        Vector3 horizontalMove = Vector3.right * _xMove * _speed * Time.deltaTime;
+        _playerRigidbody.MovePosition(transform.position + forwardMove + horizontalMove);
+    }
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Cube"))
+        {
+            _cubeList.Add(other.gameObject);
+            if (_cubeList.Count==1)
+            {
+                _firstCubePos = GetComponent<MeshRenderer>().bounds.max;
+                _currentCubePos = new Vector3(other.transform.position.x, _firstCubePos.y, other.transform.position.z);
+                other.gameObject.transform.position = _currentCubePos;
+                _currentCubePos = new Vector3(other.transform.position.x, transform.position.y + 0.3f, other.transform.position.z);
+                other.gameObject.GetComponent<Cube>().UpdateCubePosition(transform, true);
+            }
+            else if (_cubeList.Count > 1)
+            {
+                other.gameObject.transform.position = _currentCubePos;
+                _currentCubePos = new Vector3(other.transform.position.x, other.gameObject.transform.position.y + 0.3f, other.transform.position.z);
+                other.gameObject.GetComponent<Cube>().UpdateCubePosition(_cubeList[_cubeListIndexCounter].transform, true);
+                _cubeListIndexCounter++;
+            }
+        }
+    }
+}
